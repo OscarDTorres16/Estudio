2883a9ead41e3cb8d801498bc21a1c6a86235ffd
Oscar Torres <16torres.oscar@gmail.com>
2025-08-20 23:04:38 -0600

    Cambios del 20-08-25

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from '@enviroments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.component.html',
 })
-export class SideMenuHeaderComponent { }
+export class SideMenuHeaderComponent {
+
+  envs = environment
+
+ }
