e49f729dbe5c56ef526b2d3d38dd459c2de89a98
Oscar <dtorres.oscar16@gmail.com>
2025-09-10 13:57:13 -0600

    cambios 10/09/2025

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,20 @@
 import { environment } from '@enviroments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
   private http = inject(HttpClient);
 
+  trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal(true);
+
   constructor() {
     this.loadTrendingGifs();
+    console.log('Servicio creado');
   }
 
   loadTrendingGifs() {
@@ -18,7 +24,10 @@ export class GifService {
         limit: '20'
       }
     }).subscribe((resp) => {
-      console.log({resp});
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.trendingGifs.set(gifs);
+      this.trendingGifsLoading.set(false);
+      console.log({gifs});
     })
 
   }
